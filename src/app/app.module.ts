f1c6429e38f01fcdbda5417c70e4d7755bf2d2ef
Tay Zheng Qiang <tay@outlook.my>
2020-02-20 12:10:31 +0800

    Import contacts plugin as provider [ App.Module.ts ]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Contacts } from '@ionic-native/contacts';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -17,6 +18,7 @@ import { AppRoutingModule } from './app-routing.module';
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    , Contacts
   ],
   bootstrap: [AppComponent]
 })
